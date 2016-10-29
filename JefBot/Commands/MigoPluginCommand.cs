75392d80ce45a9cf933189f47c8544a15c74714b
Sean Duthie <duthie.sean@gmail.com>
2016-10-29 21:42:30 +0100

    Mod request, allow mods to do may-mays at any time.

--- JefBot/Commands/MigoPluginCommand.cs
+++ JefBot/Commands/MigoPluginCommand.cs
@@ -28,7 +28,7 @@ namespace JefBot.Commands
         }
         public void Execute(ChatCommand command, TwitchClient client)
         {
-            if (timestamp.AddMinutes(minutedelay) < DateTime.UtcNow)
+            if (timestamp.AddMinutes(minutedelay) < DateTime.UtcNow || command.ChatMessage.IsModerator)
             {
                 timestamp = DateTime.UtcNow;
                 if (File.Exists(quotefile))
