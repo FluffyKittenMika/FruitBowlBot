67bc5024b8c3e6d4592b9f9e056e23e579bee7c4
Callum R. Davies <crd@crdavies.eu>
2017-06-13 22:47:40 +0100

    Some months have 31 days.

--- JefBot/Commands/NextStreamPluginCommand.cs
+++ JefBot/Commands/NextStreamPluginCommand.cs
@@ -64,7 +64,7 @@ namespace JefBot.Commands
 
         private string GetSuffix(int day)
         {
-            return (day == 1 || day == 21) ? "st" : (day == 2 || day == 22) ? "nd" : (day == 3 || day == 23) ? "rd" : "th";
+            return (day == 1 || day == 21 || day == 31) ? "st" : (day == 2 || day == 22) ? "nd" : (day == 3 || day == 23) ? "rd" : "th";
         }
     }
 }
